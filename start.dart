98595dbed6145d8e446dc0e5ef063dfb40848a85
aliamanvermez <maliamanvermez@gmail.com>
2024-01-26 16:59:13 +0300

    first lesson

---
+++ start.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  print('Hello World');
+
+  int userMoney = 10;
+
+  int user2Money = 20;
+
+  int totalMoney = userMoney + user2Money;
+
+  print("your total money is $totalMoney");
+
+  double userMoney2 = 10.5;
+
+  double user2Money2 = 20.5;
+
+  double totalMoney2 = userMoney2 + user2Money2;
+
+  print("your total money is $totalMoney2");
+
+  print("Tebrikler paranız arttı !!!!");
+}
